e1db1c6f0f2af9176dd386b3e4cf5fa0af861f90
FanoLede <89884329+FanoLede@users.noreply.github.com>
2021-11-27 21:21:54 -0300

    Modificaciones en pruebas Coach cambio en procesos asignados

--- PruebasAdmin.py
+++ PruebasAdmin.py
@@ -1,6 +1,5 @@
+
 import time
-import unittest
-#import pytest
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.common.keys import Keys
